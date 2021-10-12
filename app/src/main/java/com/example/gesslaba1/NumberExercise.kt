87e2a25591c993a6a212aa873ac5b557d4c74623
elgess <gessalexandra@yandex.ru>
2021-10-12 14:03:07 +0300

    Исправление

--- app/src/main/java/com/example/gesslaba1/NumberExercise.kt
+++ app/src/main/java/com/example/gesslaba1/NumberExercise.kt
@@ -1,18 +1,14 @@
 package com.example.gesslaba1
 
-import java.util.*
 import kotlin.random.Random
 
 class NumberExercise {
     private val numbers: List<Double> = List(10){ Random.nextDouble(1.0 , 50.0) }
 
     fun normalize() : String {
-        val temp : Vector<Double> = Vector()
         val max = numbers.max()
         val min = numbers.min()
-        for (it in numbers){
-            temp.add((it - min!!)/(max!! - min!!))
-        }
-        return temp.toString()
+        return numbers.map{ it to (it - min!!)/(max!! - min!!)}.toMap().values.toString()
     }
 }
+
